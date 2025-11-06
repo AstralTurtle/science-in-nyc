eb7deb42b3552aa78c3c6836fc31fae2a04b69c8
Aaron J <jamesa@astralturtle.xyz>
2025-11-06 15:41:59 -0500

    add ruff

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
+import pandas as pd  # noqa: D100
 from datasets import load_dataset
-import pandas as pd
 
-from utils.settings import get_settings, Settings
+from utils.settings import Settings, get_settings
 
-settings: Settings= get_settings()
+settings: Settings = get_settings()
 
-def main():
+
+def main():  
     # github data
-    github_data = load_dataset("ibragim-bad/github-repos-metadata-40M", split=settings.github_split)
+    github_data = load_dataset(
+        "ibragim-bad/github-repos-metadata-40M", split=settings.github_split
+    )
     github_data_df = github_data.to_pandas()
     github_data_df = clean_dataset(github_data_df)
 
-    # filter ' ' languages to be N/A, this is usually due to a repo having multiple languages
-    github_data_df['language'] = github_data_df['language'].astype(object).where(
-        github_data_df['language'].notna(), pd.NA
+    # filter ' ' languages to be N/A, this is usually due 
+    # to a repo having multiple languages
+    github_data_df["language"] = (
+        github_data_df["language"]
+        .astype(object)
+        .where(github_data_df["language"].notna(), pd.NA)
     )
-    github_data_df['language'] = github_data_df['language'].apply(lambda s: s.strip() if isinstance(s, str) else s)
-    github_data_df['language'].replace('', pd.NA, inplace=True)
+    github_data_df["language"] = github_data_df["language"].apply(
+        lambda s: s.strip() if isinstance(s, str) else s
+    )
+    github_data_df["language"].replace("", pd.NA, inplace=True)
 
-    github_data_df.dropna(subset=["language"], inplace=True) # we need only repos with code with a primary programming language
+    github_data_df.dropna(
+        subset=["language"], inplace=True
+    )  # we need only repos with code with a primary programming language
 
     # debug prints: check all links
     # none_per_col = github_data_df.isna().sum()
@@ -29,10 +39,8 @@ def main():
     # else:
     #     print("No columns contain None/NaN.")
 
-
     # stack overflow data time
 
-    
     stack_overflow_df = pd.read_csv(settings.survey_csv)
     stack_overflow_df = clean_dataset(stack_overflow_df)
 
@@ -45,17 +53,13 @@ def main():
         neg_mask = stack_overflow_df[col] < 0
         if neg_mask.any():
             stack_overflow_df.loc[neg_mask, col] = pd.NA
-    
+
     stack_overflow_df.dropna(inplace=True)
 
-    
 
 def clean_dataset(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    helper for things that needs to be done on both datasets.
-    """
+    """Preforms common data cleaning tasks."""
     ret = df.copy()
-    
 
     ret.drop_duplicates()
 
@@ -63,9 +67,8 @@ def clean_dataset(df: pd.DataFrame) -> pd.DataFrame:
     for col in num_cols:
         ret[col].fillna(0, inplace=True)
 
-
-
     return ret
+
 
 if __name__ == "__main__":
     main()
